43ee7d205d43e033a9f12ef9e180692a712e5cf7
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2014-06-26 17:41:16 +0000

    Bug instalacion spidev

--- rpi/interface/setup/setup.sh
+++ rpi/interface/setup/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd
- 
+cd $BASEDIR
+
 echo "Activando interfaz SPI..."
 sudo mv /etc/modules /etc/modules_bk
 sudo ln -s $BASEDIR/config/modules /etc/modules
@@ -10,6 +10,6 @@ sudo mv /etc/modprobe.d/raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist_bk.
 sudo ln -s $BASEDIR/config/raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist.conf
 
 echo "Instalando módulo Python para acceder a la interfaz SPI..."
-sudo apt-get -qq update
 sudo apt-get -qq -y install python-dev
-sudo python $BASEDIR/py-spidev/setup.py install
+cd py_spidev
+sudo python setup.py install
