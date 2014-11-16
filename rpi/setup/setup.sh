dd2249202a57764bd91c3740dd312b82e233c5bb
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2014-11-16 12:55:56 +0000

    Añadido configuración Arduino

--- rpi/setup/setup.sh
+++ rpi/setup/setup.sh
@@ -2,18 +2,18 @@
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $HOME
  
-echo "Actualizando las listas de paquetes..."
+echo "Updating package repositories..."
 sudo apt-get -qq update
 
-echo "Actualizando los paquetes instalados..."
+echo "Updating installed packages..."
 sudo apt-get -qq upgrade
 
-echo "Instalando screen..."
+echo "Installing screen..."
 sudo apt-get -qq -y install screen
-echo "Descargando fichero de configuración de screen..."
+echo "Downloading screen's config file..."
 wget raw.github.com/startup-class/dotfiles/master/.screenrc -O .screenrc
 
-echo "Instalando emacs..."
+echo "Installing emacs..."
 sudo apt-get -qq -y install emacs
 ln -s $BASEDIR/config/.emacs ~/.emacs
 
@@ -31,16 +31,12 @@ ln -s $BASEDIR/config/.emacs ~/.emacs
 #echo "Instalando Netatalk"
 #sudo apt-get -qq -y install netatalk
 
-echo "Instalando Avahi.."
+echo "Installing Avahi.."
 sudo apt-get -qq -y install avahi-daemon
 sudo ln -s $BASEDIR$/config/afpd.service /etc/avahi/services/afpd.service
 
 #echo "Instalando Pip..."
 #sudo apt-get -qq -y install python-pip
-
-#echo "Instalando Script para administrar la consola serie..."
-#sudo wget https://raw.github.com/lurch/rpi-serial-console/master/rpi-serial-console -O /usr/bin/rpi-serial-console
-#sudo chmod +x /usr/bin/rpi-serial-console
 
 #echo "Instalando paquete Python: pyserial..."
 #sudo pip install pyserial
@@ -64,3 +60,29 @@ sudo apt-get -qq -y install python-sphinx
 
 echo "Instalando graphviz.."
 sudo apt-get -qq -y install graphviz
+
+echo "Install Arduino IDE..."
+sudo apt-get -qq -y install arduino
+
+echo "Install modified flash loader..."
+wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb -O /tmp/avrdude_5.10-4_armhf.deb
+sudo dpkg -i /tmp/avrdude_5.10-4_armhf.deb
+sudo chmod 4755 /usr/bin/avrdude
+
+echo "Add Arduino IDE definitions for Gertboard and Raspberry Pi..."
+ARDUINO_CONF_PATH=/usr/share/arduino/hardware/arduino
+cat config/gert328_board.txt $ARDUINO_CONF_PATH$/boards.txt > /tmp/boards.txt
+sudo mv $ARDUINO_CONF_PATH$/boards.txt $ARDUINO_CONF_PATH$/boards.txt.bak
+sudo mv /tmp/boards.txt $ARDUINO_CONF_PATH$/
+cat config/gpio_programmer.txt $ARDUINO_CONF_PATH$/programmers.txt > /tmp/programmers.txt
+sudo mv $ARDUINO_CONF_PATH$/programmers.txt $ARDUINO_CONF_PATH$/programmers.txt.bak
+sudo mv /tmp/programmers.txt $ARDUINO_CONF_PATH$/
+
+echo "Download script for ATmega328 initialisation..."
+sudo rm -f /usr/local/bin/avrsetup
+sudo wget -q http://project-downloads.drogon.net/gertboard/avrsetup -O /usr/local/bin/avrsetup
+sudo chmod 755 /usr/local/bin/avrsetup
+
+echo "Download script for enabling/disabling serial console..."
+sudo wget https://raw.githubusercontent.com/lurch/rpi-serial-console/master/rpi-serial-console -O /usr/bin/rpi-serial-console
+sudo chmod +x /usr/bin/rpi-serial-console
