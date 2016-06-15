f5c3776c748c7f42704428bfdd1399caef30ef0e
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2016-06-15 23:13:42 +0000

    Python development environment configuration added

--- rpi/setup/setup.sh
+++ rpi/setup/setup.sh
@@ -1,88 +1,94 @@
 #!/bin/bash
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $HOME
- 
-echo "Updating package repositories..."
-sudo apt-get -qq update
 
-echo "Updating installed packages..."
-sudo apt-get -qq upgrade
+echo "Configuring Python development environment"
+mkdir .virtualenvs
+sudo pip install virtualenvwrapper
+ln -s $BASEDIR/config/.profile .profile
+. .profile
 
-echo "Installing screen..."
-sudo apt-get -qq -y install screen
-echo "Downloading screen's config file..."
-wget raw.github.com/startup-class/dotfiles/master/.screenrc -O .screenrc
+#echo "Updating package repositories..."
+#sudo apt-get -qq update
 
-echo "Installing emacs..."
-sudo apt-get -qq -y install emacs
-ln -s $BASEDIR/config/.emacs ~/.emacs
+#echo "Updating installed packages..."
+#sudo apt-get -qq upgrade
 
-#echo "Activando interfaz SPI..."
-#sudo mv /etc/modules /etc/modules_bk
-#sudo ln -s $BASEDIR/config/modules /etc/modules
-#sudo mv /etc/modprobe.d/raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist_bk.conf
-#sudo ln -s $BASEDIR/config/raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist.conf
+#echo "Installing screen..."
+#sudo apt-get -qq -y install screen
+#echo "Downloading screen's config file..."
+#wget raw.github.com/startup-class/dotfiles/master/.screenrc -O .screenrc
 
-#echo "Instalando módulo Python para acceder a la interfaz SPI..."
-#sudo apt-get -qq -y install python-dev
-#git clone git://github.com/doceme/py-spidev
-#sudo python py-spidev/setup.py install
+#echo "Installing emacs..."
+#sudo apt-get -qq -y install emacs
+#ln -s $BASEDIR/config/.emacs ~/.emacs
 
-#echo "Instalando Netatalk"
-#sudo apt-get -qq -y install netatalk
+##echo "Activando interfaz SPI..."
+##sudo mv /etc/modules /etc/modules_bk
+##sudo ln -s $BASEDIR/config/modules /etc/modules
+##sudo mv /etc/modprobe.d/raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist_bk.conf
+##sudo ln -s $BASEDIR/config/raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist.conf
 
-echo "Installing Avahi.."
-sudo apt-get -qq -y install avahi-daemon
-sudo ln -s $BASEDIR$/config/afpd.service /etc/avahi/services/afpd.service
+##echo "Instalando módulo Python para acceder a la interfaz SPI..."
+##sudo apt-get -qq -y install python-dev
+##git clone git://github.com/doceme/py-spidev
+##sudo python py-spidev/setup.py install
 
-#echo "Instalando Pip..."
-#sudo apt-get -qq -y install python-pip
+##echo "Instalando Netatalk"
+##sudo apt-get -qq -y install netatalk
 
-#echo "Instalando paquete Python: pyserial..."
-#sudo pip install pyserial
+#echo "Installing Avahi.."
+#sudo apt-get -qq -y install avahi-daemon
+#sudo ln -s $BASEDIR$/config/afpd.service /etc/avahi/services/afpd.service
 
-#echo "Instalando herramientas de programación AVR..."
-#sudo apt-get -qq -y install avrdude avrdude-doc binutils-avr avr-libc gcc-avr gdb-avr
+##echo "Instalando Pip..."
+##sudo apt-get -qq -y install python-pip
 
-#echo "Instalando programador/flash AVR..."
-#wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb
-#sudo dpkg -i avrdude_5.10-4_armhf.deb
-#rm avrdude_5.10-4_armhf.deb
+##echo "Instalando paquete Python: pyserial..."
+##sudo pip install pyserial
 
-echo "Installing tightvncserver (asks for password)..."
-sudo apt-get -qq -y install tightvncserver
-vncserver :1
-mkdir -p .config/autostart
-ln -s $BASEDIR$/config/tightvnc.desktop ~/.config/autostart/tightvnc.desktop
+##echo "Instalando herramientas de programación AVR..."
+##sudo apt-get -qq -y install avrdude avrdude-doc binutils-avr avr-libc gcc-avr gdb-avr
 
-echo "Installing sphinx..."
-sudo apt-get -qq -y install python-sphinx
+##echo "Instalando programador/flash AVR..."
+##wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb
+##sudo dpkg -i avrdude_5.10-4_armhf.deb
+##rm avrdude_5.10-4_armhf.deb
 
-echo "Installing graphviz.."
-sudo apt-get -qq -y install graphviz
+#echo "Installing tightvncserver (asks for password)..."
+#sudo apt-get -qq -y install tightvncserver
+#vncserver :1
+#mkdir -p .config/autostart
+#ln -s $BASEDIR$/config/tightvnc.desktop ~/.config/autostart/tightvnc.desktop
 
-echo "Installing Arduino IDE..."
-sudo apt-get -qq -y install arduino
+#echo "Installing sphinx..."
+#sudo apt-get -qq -y install python-sphinx
 
-echo "Installing modified AVR flash loader..."
-wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb -O /tmp/avrdude_5.10-4_armhf.deb
-sudo dpkg -i /tmp/avrdude_5.10-4_armhf.deb
-sudo chmod 4755 /usr/bin/avrdude
+#echo "Installing graphviz.."
+#sudo apt-get -qq -y install graphviz
 
-echo "Adding Arduino IDE definitions for Gertboard and Raspberry Pi..."
-ARDUINO_CONF_PATH=/usr/share/arduino/hardware/arduino
-cat $BASEDIR/config/gert328_board.txt $ARDUINO_CONF_PATH$/boards.txt > /tmp/boards.txt
-sudo mv $ARDUINO_CONF_PATH$/boards.txt $ARDUINO_CONF_PATH$/boards.txt.bak
-sudo mv /tmp/boards.txt $ARDUINO_CONF_PATH$/
-cat $BASEDIR/config/gpio_programmer.txt $ARDUINO_CONF_PATH$/programmers.txt > /tmp/programmers.txt
-sudo mv $ARDUINO_CONF_PATH$/programmers.txt $ARDUINO_CONF_PATH$/programmers.txt.bak
-sudo mv /tmp/programmers.txt $ARDUINO_CONF_PATH$/
+#echo "Installing Arduino IDE..."
+#sudo apt-get -qq -y install arduino
 
-echo "Downloading script for ATmega328 initialisation..."
-sudo rm -f /usr/local/bin/avrsetup
-sudo wget -q http://project-downloads.drogon.net/gertboard/avrsetup -O /usr/local/bin/avrsetup
-sudo chmod 755 /usr/local/bin/avrsetup
+#echo "Installing modified AVR flash loader..."
+#wget http://project-downloads.drogon.net/gertboard/avrdude_5.10-4_armhf.deb -O /tmp/avrdude_5.10-4_armhf.deb
+#sudo dpkg -i /tmp/avrdude_5.10-4_armhf.deb
+#sudo chmod 4755 /usr/bin/avrdude
 
-echo "Downloading script for enabling/disabling serial console..."
-sudo wget https://raw.githubusercontent.com/lurch/rpi-serial-console/master/rpi-serial-console -O /usr/bin/rpi-serial-console
-sudo chmod +x /usr/bin/rpi-serial-console
+#echo "Adding Arduino IDE definitions for Gertboard and Raspberry Pi..."
+#ARDUINO_CONF_PATH=/usr/share/arduino/hardware/arduino
+#cat $BASEDIR/config/gert328_board.txt $ARDUINO_CONF_PATH$/boards.txt > /tmp/boards.txt
+#sudo mv $ARDUINO_CONF_PATH$/boards.txt $ARDUINO_CONF_PATH$/boards.txt.bak
+#sudo mv /tmp/boards.txt $ARDUINO_CONF_PATH$/
+#cat $BASEDIR/config/gpio_programmer.txt $ARDUINO_CONF_PATH$/programmers.txt > /tmp/programmers.txt
+#sudo mv $ARDUINO_CONF_PATH$/programmers.txt $ARDUINO_CONF_PATH$/programmers.txt.bak
+#sudo mv /tmp/programmers.txt $ARDUINO_CONF_PATH$/
+
+#echo "Downloading script for ATmega328 initialisation..."
+#sudo rm -f /usr/local/bin/avrsetup
+#sudo wget -q http://project-downloads.drogon.net/gertboard/avrsetup -O /usr/local/bin/avrsetup
+#sudo chmod 755 /usr/local/bin/avrsetup
+
+#echo "Downloading script for enabling/disabling serial console..."
+#sudo wget https://raw.githubusercontent.com/lurch/rpi-serial-console/master/rpi-serial-console -O /usr/bin/rpi-serial-console
+#sudo chmod +x /usr/bin/rpi-serial-console
