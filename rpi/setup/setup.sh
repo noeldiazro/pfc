9272c4158789fbab39deeffb6fae28c0ebf33684
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2017-06-12 21:16:13 +0000

    Añadida activación de SPI

--- rpi/setup/setup.sh
+++ rpi/setup/setup.sh
@@ -13,6 +13,9 @@ echo ">>> Installing emacs...<<<"
 sudo apt-get -qq -y install emacs
 ln -s $BASEDIR/config/.emacs .emacs
 
+echo ">>> Installing netatalk...<<<"
+sudo apt-get -qq -y install netatalk
+
 echo ">>> Configuring Python development environment... <<<"
 sudo apt-get -qq -y install python-dev
 mkdir dev
@@ -21,5 +24,12 @@ sudo pip install virtualenvwrapper
 
 echo ">>> Configuring bash... <<<"
 cp $BASEDIR/config/.bashrc .bashrc
+
+echo ">>> Enabling SPI... <<<"
+sudo cp $BASEDIR/config/config.txt /boot
+sudo cp $BASEDIR/config/modules /etc
+
+echo ">>> Installing cyclictest... <<<"
+sudo apt-get -qq -y install rt-tests
 
 echo ">>> Reboot is required for changes to take effect. <<<"
